3955989042c4ff8139691c471b217172c264fcd0
Vyom Mani Tiwari <vyommani@gmail.com>
2025-09-24 23:41:37 +0530

    removed b from reserved list

--- src/config.rs
+++ src/config.rs
@@ -64,7 +64,7 @@ pub mod parser {
         pub static ref RESERVED_KEYWORDS: HashSet<String> = [
             "ret", "global", "mov", "rax", "rbx", "rcx", "rdx", "rsi", "rdi", "rbp", "rsp",
             "r8", "r9", "r10", "r11", "r12", "r13", "r14", "r15",
-            "mov", "ldr", "str", "b", "bl", "x0", "x1", "x2", "x3", "x4", "x5", "x6", "x7",
+            "mov", "ldr", "str", "bl", "x0", "x1", "x2", "x3", "x4", "x5", "x6", "x7",
             "x8", "x9", "x10", "x11", "x12", "x13", "x14", "x15",
             "x16", "x17", "x18", "x19", "x20", "x21", "x22", "x23",
             "x24", "x25", "x26", "x27", "x28", "x29", "x30", "sp", "lr", "fp",
