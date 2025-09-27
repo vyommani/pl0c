802a5323600f4aeb5a15d7d54468182efbacd718
Vyom Mani Tiwari <vyommani@gmail.com>
2025-09-27 20:37:33 +0530

    refactored lexer and added unit test for lexer.

--- src/lexer.rs
+++ src/lexer.rs
@@ -2,201 +2,230 @@ use crate::LineNumber;
 use crate::{symboltable::SymbolTable, token::Token};
 use std::{iter::Peekable, str::Chars};
 use crate::errors::{Pl0Error, Pl0Result};
-pub fn scan(state: &mut LineNumber, file_content: &str, _table: &mut SymbolTable) -> Pl0Result<Vec<(Token, usize)>> {
-    let mut chars = file_content.chars().peekable();
-    let mut lexeme: Vec<(Token, usize)> = vec![];
-    let mut lookahead = false;
 
-    'lexer: loop {
-        if let Some(c) = chars.peek() {
-            if (*c).eq(&'{') {
-                let _ = comment(&mut chars, state);
-            } else if (*c).is_whitespace() {
-                whitespace(&mut chars, state);
-            } else if (*c).is_alphabetic() || (*c).eq(&'_') {
-                let token = identifier(&mut chars, state)?;
-                lexeme.push((token, state.line));
-            } else if (*c).is_numeric() {
-                let token = number(&mut chars, state)?;
-                lexeme.push((token, state.line));
-            } else if (*c).eq(&':') {
-                let token = assignment(&mut chars, state)?;
-                lexeme.push((token, state.line));
-            } else {
-                let token = match *c {
-                    '.' => Token::Dot,
-                    '=' => Token::Equal,
-                    ',' => Token::Comma,
-                    ';' => Token::Semicolon,
-                    '#' => Token::Hash,
-                    '+' => Token::Plus,
-                    '-' => Token::Minus,
-                    '*' => Token::Multiply,
-                    '/' => Token::Divide,
-                    '%' => Token::Modulo,
-                    '(' => Token::LParen,
-                    ')' => Token::RParen,
-                    '[' => Token::LBrack,
-                    ']' => Token::RBrack,
-                    '<' => {
-                        lookahead = true;
-                        chars.next(); // look ahead one charcter
-                        if let Some(c) = chars.peek() {
-                            if *c == '=' {
-                                chars.next(); //consume the '=' character
-                                Token::LessThanEqual
-                            } else if *c == '>' {
-                                chars.next(); //consume the '>' character
-                                Token::Hash
-                            } else {
-                                Token::LessThan
-                            }
-                        } else {
-                            Token::LessThan
-                        }
-                    }
-                    '>' => {
-                        lookahead = true;
-                        chars.next(); // look ahead one charcter
-                        if chars.peek() == Some(&'=') {
-                            chars.next(); //consume the '=' character
-                            Token::GreaterThanEqual
-                        } else {
-                            Token::GreaterThan
-                        }
-                    }
-                    ':' => {
-                        lookahead = true;
-                        chars.next(); // look ahead one charcter
-                        if chars.peek() != Some(&'=') {
-                            return Err(Pl0Error::UnknownToken {token: ':',line: state.line,});
-                        }
-                        chars.next(); // consume the '=' character.
-                        Token::Assign
-                    }
-                    '\'' => match get_string_literal(&mut chars, state) {
-                        Ok(literal) => literal,
-                        Err(_) => Token::StringLiteral("".to_string()),
-                    },
-                    '\0' => Token::Null,
-                    _ => {
-                        return Err(Pl0Error::UnknownToken {token: ':',line: state.line,});
-                    }
-                };
-                lexeme.push((token, state.line));
-                if lookahead == true {
-                    lookahead = false;
-                } else {
-                    chars.next();
+pub struct Lexer<'a> {
+    chars: Peekable<Chars<'a>>,
+    state: &'a mut LineNumber,
+}
+
+impl<'a> Lexer<'a> {
+    pub fn new(file_content: &'a str, state: &'a mut LineNumber) -> Self {
+        Self {
+            chars: file_content.chars().peekable(),
+            state,
+        }
+    }
+
+    pub fn scan(mut self, _table: &mut SymbolTable) -> Pl0Result<Vec<(Token, usize)>> {
+        let mut tokens = Vec::new();
+        while self.chars.peek().is_some() {
+            self.skip_whitespace_and_comments()?;
+            if let Some(&ch) = self.chars.peek() {
+                let line = self.state.line;
+                let token = self.scan_token()?;
+                if let Some(token) = token {
+                    tokens.push((token, line));
                 }
             }
-        } else {
-            break 'lexer;
         }
+        Ok(tokens)
     }
-    Ok(lexeme)
-}
 
-fn comment(chars: &mut Peekable<Chars<'_>>, state: &mut LineNumber) -> Pl0Result<()> {
-    chars.next(); // Consume '{'
-    let line = state.line;
-    for c in chars.by_ref() {
-        if c == '\n' {
-            state.line += 1;
-        } else if c == '}' {
-            return Ok(());
-        }
-    }
-    Err(Pl0Error::UnterminatedComment { line })
-}
-
-pub fn get_string_literal(chars: &mut Peekable<Chars<'_>>, state: &mut LineNumber) -> Pl0Result<Token> {
-    let mut string_literal = String::new();
-    chars.next(); // consume the opening '
-    loop {
-        if let Some(c) = chars.peek() {
-            if (*c) == '\'' {
-                break;
-            } else if (*c) == '\n' {
-                state.line += 1;
-                return Err(Pl0Error::MultilineString { line: state.line });
-            } else if (*c) == '\0' {
-                return Err(Pl0Error::UnterminatedString { line: state.line });
-            } else {
-                string_literal.push(*c);
-                chars.next();
+    fn skip_whitespace_and_comments(&mut self) -> Pl0Result<()> {
+        loop {
+            match self.chars.peek() {
+                Some(&'{') => self.scan_comment()?,
+                Some(&ch) if ch.is_whitespace() => self.scan_whitespace(),
+                _ => break,
             }
-        } else {
-            return Err(Pl0Error::UnterminatedString { line: state.line });
+        }
+        Ok(())
+    }
+
+    fn scan_token(&mut self) -> Pl0Result<Option<Token>> {
+        match self.chars.peek() {
+            None => Ok(None),
+            Some(&ch) if ch.is_alphabetic() || ch == '_' => Ok(Some(self.scan_identifier()?)),
+            Some(&ch) if ch.is_ascii_digit() => Ok(Some(self.scan_number()?)),
+            Some(&'\'') => Ok(Some(self.scan_string_literal('\'')?)),
+            Some(&'"') => Ok(Some(self.scan_string_literal('"')?)),
+            Some(&':') => Ok(Some(self.scan_assignment()?)),
+            Some(&'<') => Ok(Some(self.scan_less_than()?)),
+            Some(&'>') => Ok(Some(self.scan_greater_than()?)),
+            Some(&ch) => Ok(Some(self.scan_single_char_token(ch)?)),
         }
     }
-    Ok(Token::StringLiteral(string_literal))
-}
 
-fn whitespace(chars: &mut Peekable<Chars<'_>>, state: &mut LineNumber) {
-    while let Some(&c) = chars.peek() {
-        if c.is_whitespace() {
-            if c == '\n' {
-                state.line += 1;
+    fn scan_comment(&mut self) -> Pl0Result<()> {
+        self.chars.next(); // Consume '{'
+        let start_line = self.state.line;
+        while let Some(ch) = self.chars.next() {
+            if ch == '\n' {
+                self.state.line += 1;
+            } else if ch == '}' {
+                return Ok(());
             }
-            chars.next();
-        } else {
-            break;
         }
+        Err(Pl0Error::UnterminatedComment { line: start_line })
     }
-}
 
-pub fn assignment(chars: &mut Peekable<Chars<'_>>, state: &mut LineNumber) -> Pl0Result<Token> {
-    chars.next();
-    if let Some(c) = chars.peek() {
-        if (*c).eq(&'=') {
-            chars.next();
-            Ok(Token::Assign)
-        } else {
-            Err(Pl0Error::lexer_error("Expected '=' after ':'", state.line))
-        }
-    } else {
-        Err(Pl0Error::lexer_error("Unexpected end of file after ':'", state.line))
-    }
-}
-
-pub fn identifier(chars: &mut Peekable<Chars<'_>>, state: &mut LineNumber) -> Pl0Result<Token> {
-    let mut idt = String::new();
-    loop {
-        if let Some(c) = chars.peek() {
-            if (*c).is_alphanumeric() || (*c).eq(&'_') {
-                idt.push(*c);
-                chars.next();
+    fn scan_whitespace(&mut self) {
+        while let Some(&ch) = self.chars.peek() {
+            if ch.is_whitespace() {
+                if ch == '\n' {
+                    self.state.line += 1;
+                }
+                self.chars.next();
             } else {
                 break;
             }
-        } else {
-            return Err(Pl0Error::UnterminatedString { line: state.line });
         }
     }
-    static KEYWORDS: &[(&str, Token)] = &[("const", Token::Const), ("var", Token::Var), ("procedure", Token::Procedure), ("call", Token::Call),
-        ("begin", Token::Begin), ("end", Token::End), ("if", Token::If), ("then", Token::Then), ("else", Token::Else), ("while", Token::While), 
-        ("do", Token::Do), ("odd", Token::Odd), ("write", Token::Write), ("writestr", Token::WriteStr), ("writeStr", Token::WriteStr),
-        ("read", Token::Read), ("into", Token::Into), ("size", Token::Size), ("exit", Token::Exit), ("and", Token::And),
-        ("or", Token::Or), ("not", Token::Not), ("mod", Token::Modulo), ("forward", Token::Forward),
-    ];
-    let binding = Token::Ident(idt.clone());
-    let token = KEYWORDS.iter().find(|&&(k, _)| k == idt).map(|(_, t)| t).unwrap_or(&binding);
-    Ok(token.clone())
+
+    fn scan_identifier(&mut self) -> Pl0Result<Token> {
+        let mut identifier = String::new();
+        while let Some(&ch) = self.chars.peek() {
+            if ch.is_alphanumeric() || ch == '_' {
+                identifier.push(ch);
+                self.chars.next();
+            } else {
+                break;
+            }
+        }
+        Ok(self.keyword_or_identifier(identifier))
+    }
+
+    fn keyword_or_identifier(&self, identifier: String) -> Token {
+        match identifier.as_str() {
+            "const" => Token::Const,
+            "var" => Token::Var,
+            "procedure" => Token::Procedure,
+            "call" => Token::Call,
+            "begin" => Token::Begin,
+            "end" => Token::End,
+            "if" => Token::If,
+            "then" => Token::Then,
+            "else" => Token::Else,
+            "while" => Token::While,
+            "do" => Token::Do,
+            "odd" => Token::Odd,
+            "write" => Token::Write,
+            "writestr" | "writeStr" => Token::WriteStr,
+            "read" => Token::Read,
+            "into" => Token::Into,
+            "size" => Token::Size,
+            "exit" => Token::Exit,
+            "and" => Token::And,
+            "or" => Token::Or,
+            "not" => Token::Not,
+            "mod" => Token::Modulo,
+            "forward" => Token::Forward,
+            _ => Token::Ident(identifier),
+        }
+    }
+
+    fn scan_number(&mut self) -> Pl0Result<Token> {
+        let mut number_str = String::new();
+        while let Some(&ch) = self.chars.peek() {
+            if ch.is_ascii_digit() {
+                number_str.push(ch);
+                self.chars.next();
+            } else {
+                break;
+            }
+        }
+        number_str.parse::<i64>()
+            .map(Token::Number)
+            .map_err(|_| Pl0Error::InvalidNumber {number: number_str, line: self.state.line})
+    }
+
+    fn scan_string_literal(&mut self, quote_char: char) -> Pl0Result<Token> {
+        self.chars.next(); // Consume opening quote
+        let mut literal = String::new();
+        loop {
+            match self.chars.peek() {
+                Some(&ch) if ch == quote_char => {
+                    self.chars.next(); // Consume closing quote
+                    break;
+                }
+                Some(&'\n') => {
+                    self.state.line += 1;
+                    return Err(Pl0Error::MultilineString { line: self.state.line });
+                }
+                Some(&ch) => {
+                    literal.push(ch);
+                    self.chars.next();
+                }
+                None => {
+                    return Err(Pl0Error::UnterminatedString { line: self.state.line });
+                }
+            }
+        }
+        Ok(Token::StringLiteral(literal))
+    }
+
+    fn scan_assignment(&mut self) -> Pl0Result<Token> {
+        self.chars.next(); // Consume ':'
+        match self.chars.peek() {
+            Some(&'=') => {
+                self.chars.next(); // Consume '='
+                Ok(Token::Assign)
+            }
+            _ => Err(Pl0Error::lexer_error("Expected '=' after ':'", self.state.line)),
+        }
+    }
+
+    fn scan_less_than(&mut self) -> Pl0Result<Token> {
+        self.chars.next(); // Consume '<'
+        match self.chars.peek() {
+            Some(&'=') => {
+                self.chars.next(); // Consume '='
+                Ok(Token::LessThanEqual)
+            }
+            Some(&'>') => {
+                self.chars.next(); // Consume '>'
+                Ok(Token::Hash)
+            }
+            _ => Ok(Token::LessThan),
+        }
+    }
+
+    fn scan_greater_than(&mut self) -> Pl0Result<Token> {
+        self.chars.next(); // Consume '>'
+        if self.chars.peek() == Some(&'=') {
+            self.chars.next(); // Consume '='
+            Ok(Token::GreaterThanEqual)
+        } else {
+            Ok(Token::GreaterThan)
+        }
+    }
+
+    fn scan_single_char_token(&mut self, ch: char) -> Pl0Result<Token> {
+        self.chars.next(); // Consume the character
+        let token = match ch {
+            '.' => Token::Dot,
+            '=' => Token::Equal,
+            ',' => Token::Comma,
+            ';' => Token::Semicolon,
+            '#' => Token::Hash,
+            '+' => Token::Plus,
+            '-' => Token::Minus,
+            '*' => Token::Multiply,
+            '/' => Token::Divide,
+            '%' => Token::Modulo,
+            '(' => Token::LParen,
+            ')' => Token::RParen,
+            '[' => Token::LBrack,
+            ']' => Token::RBrack,
+            '\0' => Token::Null,
+            _ => return Err(Pl0Error::UnknownToken {token: ch, line: self.state.line}),
+        };
+        Ok(token)
+    }
 }
 
-pub fn number(chars: &mut Peekable<Chars<'_>>, state: &mut LineNumber) -> Pl0Result<Token> {
-    let mut number_str = String::new();
-    while let Some(c) = chars.peek() {
-        if c.is_numeric() {
-            number_str.push(*c);
-            chars.next();
-        } else {
-            break;
-        }
-    }
-    match number_str.parse::<i64>() {
-        Ok(num) => Ok(Token::Number(num)),
-        Err(_) => Err(Pl0Error::InvalidNumber {number: number_str, line: state.line,}),
-    }
+// Convenience function to maintain the original API
+pub fn scan(state: &mut LineNumber, file_content: &str, table: &mut SymbolTable) -> Pl0Result<Vec<(Token, usize)>> {
+    let lexer = Lexer::new(file_content, state);
+    lexer.scan(table)
 }
---
+++ tests/lexer_tests.rs
@@ -0,0 +1,301 @@
+use pl0c::errors::{Pl0Error, Pl0Result};
+use pl0c::lexer::{scan};
+use pl0c::symboltable::SymbolTable;
+use pl0c::token::Token;
+use pl0c::LineNumber;
+
+#[test]
+fn test_number_literals() -> Pl0Result<()> {
+    let source = "123 456 0 999999";
+    let mut state = LineNumber::default();
+    let mut table = SymbolTable::new();
+    let tokens = scan(&mut state, source, &mut table)?;
+    assert_eq!(
+        tokens,
+        vec![
+            (Token::Number(123), 1),
+            (Token::Number(456), 1),
+            (Token::Number(0), 1),
+            (Token::Number(999999), 1),
+        ]
+    );
+    Ok(())
+}
+
+#[test]
+fn test_string_literal() -> Pl0Result<()> {
+    let source = "writestr(\"Hello, World!\") .";
+    let mut state = LineNumber::default();
+    let mut table = SymbolTable::new();
+    let tokens = scan(&mut state, source, &mut table)?;
+    assert_eq!(
+        tokens,
+        vec![
+            (Token::WriteStr, 1),
+            (Token::LParen, 1),
+            (Token::StringLiteral("Hello, World!".to_string()), 1),
+            (Token::RParen, 1),
+            (Token::Dot, 1),
+        ]
+    );
+    Ok(())
+}
+
+#[test]
+fn test_unterminated_string_literal() -> Pl0Result<()> {
+    let source = "\"this is unterminated";
+    let mut state = LineNumber::default();
+    let mut table = SymbolTable::new();
+    let result = scan(&mut state, source, &mut table);
+    if let Err(Pl0Error::UnterminatedString { line }) = result {
+        assert_eq!(line, 1);
+        Ok(())
+    } else {
+        panic!("Expected a UnterminatedString for unterminated string, but got: {:?}", result);
+    }
+}
+
+#[test]
+fn test_multiline_string_literal_error() -> Pl0Result<()> {
+    let source = "\"this is not\na valid string\"";
+    let mut state = LineNumber::default();
+    let mut table = SymbolTable::new();
+    let result = scan(&mut state, source, &mut table);
+    if let Err(Pl0Error::MultilineString { line }) = result {
+        assert_eq!(line, 2);
+        Ok(())
+    } else {
+        panic!("Expected a MultilineString error, but got: {:?}", result);
+    }
+}
+
+#[test]
+fn test_single_line_comment() -> Pl0Result<()> {
+    let source = "var x; { This is a comment } x := 1;";
+    let mut state = LineNumber::default();
+    let mut table = SymbolTable::new();
+    let tokens = scan(&mut state, source, &mut table)?;
+    assert_eq!(
+        tokens,
+        vec![
+            (Token::Var, 1),
+            (Token::Ident("x".to_string()), 1),
+            (Token::Semicolon, 1),
+            (Token::Ident("x".to_string()), 1),
+            (Token::Assign, 1),
+            (Token::Number(1), 1),
+            (Token::Semicolon, 1),
+        ]
+    );
+    Ok(())
+}
+
+#[test]
+fn test_multi_line_comment() -> Pl0Result<()> {
+    let source = "
+var x; { This is a
+multi-line
+comment } x := 1;
+";
+    let mut state = LineNumber::default();
+    let mut table = SymbolTable::new();
+    let tokens = scan(&mut state, source, &mut table)?;
+    assert_eq!(
+        tokens,
+        vec![
+            (Token::Var, 2),
+            (Token::Ident("x".to_string()), 2),
+            (Token::Semicolon, 2),
+            (Token::Ident("x".to_string()), 4),
+            (Token::Assign, 4),
+            (Token::Number(1), 4),
+            (Token::Semicolon, 4),
+        ]
+    );
+    Ok(())
+}
+
+
+#[test]
+fn test_unterminated_comment() -> Pl0Result<()> {
+    let source = "var x; { This is an unterminated comment\nx := 1;";
+    let mut state = LineNumber::default();
+    let mut table = SymbolTable::new();
+    let result = scan(&mut state, source, &mut table);
+    if let Err(Pl0Error::UnterminatedComment { line }) = result {
+        assert_eq!(line, 1);
+        Ok(())
+    } else {
+        panic!("Expected an UnterminatedComment error, but got: {:?}", result);
+    }
+}
+
+#[test]
+fn test_complex_operators() -> Pl0Result<()> {
+    let source = "if x <> y then z := x + y else z := x - y; .";
+    let mut state = LineNumber::default();
+    let mut table = SymbolTable::new();
+    let tokens = scan(&mut state, source, &mut table)?;
+    assert_eq!(
+        tokens,
+        vec![
+            (Token::If, 1),
+            (Token::Ident("x".to_string()), 1),
+            (Token::Hash, 1),
+            (Token::Ident("y".to_string()), 1),
+            (Token::Then, 1),
+            (Token::Ident("z".to_string()), 1),
+            (Token::Assign, 1),
+            (Token::Ident("x".to_string()), 1),
+            (Token::Plus, 1),
+            (Token::Ident("y".to_string()), 1),
+            (Token::Else, 1),
+            (Token::Ident("z".to_string()), 1),
+            (Token::Assign, 1),
+            (Token::Ident("x".to_string()), 1),
+            (Token::Minus, 1),
+            (Token::Ident("y".to_string()), 1),
+            (Token::Semicolon, 1),
+            (Token::Dot, 1),
+        ]
+    );
+    Ok(())
+}
+
+#[test]
+fn test_relational_operators() -> Pl0Result<()> {
+    let source = "if a > b OR a < b OR a >= b OR a <= b then a := 10;.";
+    let mut state = LineNumber::default();
+    let mut table = SymbolTable::new();
+    let tokens = scan(&mut state, source, &mut table)?;
+    assert_eq!(
+        tokens,
+        vec![
+            (Token::If, 1),
+            (Token::Ident("a".to_string()), 1),
+            (Token::GreaterThan, 1),
+            (Token::Ident("b".to_string()), 1),
+            (Token::Ident("OR".to_string()), 1),
+            (Token::Ident("a".to_string()), 1),
+            (Token::LessThan, 1),
+            (Token::Ident("b".to_string()), 1),
+            (Token::Ident("OR".to_string()), 1),
+            (Token::Ident("a".to_string()), 1),
+            (Token::GreaterThanEqual, 1),
+            (Token::Ident("b".to_string()), 1),
+            (Token::Ident("OR".to_string()), 1),
+            (Token::Ident("a".to_string()), 1),
+            (Token::LessThanEqual, 1),
+            (Token::Ident("b".to_string()), 1),
+            (Token::Then, 1),
+            (Token::Ident("a".to_string()), 1),
+            (Token::Assign, 1),
+            (Token::Number(10), 1),
+            (Token::Semicolon, 1),
+            (Token::Dot, 1),
+        ]
+    );
+    Ok(())
+}
+
+#[test]
+fn test_full_program() -> Pl0Result<()> {
+    let source = "
+    var x, y;
+    begin
+        read(x);
+        if x > 0 then
+            y := 1;
+        else
+            y := 0;
+        write(y);
+    end.
+    ";
+    let mut state = LineNumber::default();
+    let mut table = SymbolTable::new();
+    let tokens = scan(&mut state, source, &mut table)?;
+    assert_eq!(
+        tokens,
+        vec![
+            (Token::Var, 2),
+            (Token::Ident("x".to_string()), 2),
+            (Token::Comma, 2),
+            (Token::Ident("y".to_string()), 2),
+            (Token::Semicolon, 2),
+            (Token::Begin, 3),
+            (Token::Read, 4),
+            (Token::LParen, 4),
+            (Token::Ident("x".to_string()), 4),
+            (Token::RParen, 4),
+            (Token::Semicolon, 4),
+            (Token::If, 5),
+            (Token::Ident("x".to_string()), 5),
+            (Token::GreaterThan, 5),
+            (Token::Number(0), 5),
+            (Token::Then, 5),
+            (Token::Ident("y".to_string()), 6),
+            (Token::Assign, 6),
+            (Token::Number(1), 6),
+            (Token::Semicolon, 6),
+            (Token::Else, 7),
+            (Token::Ident("y".to_string()), 8),
+            (Token::Assign, 8),
+            (Token::Number(0), 8),
+            (Token::Semicolon, 8),
+            (Token::Write, 9),
+            (Token::LParen, 9),
+            (Token::Ident("y".to_string()), 9),
+            (Token::RParen, 9),
+            (Token::Semicolon, 9),
+            (Token::End, 10),
+            (Token::Dot, 10),
+        ]
+    );
+    Ok(())
+}
+
+#[test]
+fn test_multiple_variables() {
+    // This test ensures the lexer correctly tokenizes a line with multiple variable
+    // declarations separated by commas, without corrupting the identifiers.
+    let source = "
+    var a, b;
+    .
+    ";
+
+    let mut state = LineNumber::default();
+    let mut symbol_table = SymbolTable::new();
+    let result = scan(&mut state, source, &mut symbol_table).unwrap();
+
+    let expected = vec![
+        (Token::Var, 2),
+        (Token::Ident("a".to_string()), 2),
+        (Token::Comma, 2),
+        (Token::Ident("b".to_string()), 2),
+        (Token::Semicolon, 2),
+        (Token::Dot, 3),
+    ];
+
+    assert_eq!(result, expected);
+}
+
+#[test]
+fn test_not_operator() {
+    
+    let source = "while x <> 100 do.";
+
+    let mut state = LineNumber::default();
+    let mut symbol_table = SymbolTable::new();
+    let result = scan(&mut state, source, &mut symbol_table).unwrap();
+
+    let expected = vec![
+        (Token::While, 1),
+        (Token::Ident("x".to_string()), 1),
+        (Token::Hash, 1),
+        (Token::Number(100), 1),
+        (Token::Do, 1),
+        (Token::Dot, 1),
+    ];
+
+    assert_eq!(result, expected);
+}
